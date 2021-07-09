6a117eceadd885248d7114f3cd3bc550051a34db
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-07-10 04:49:33 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,35 +1,24 @@
 import React from 'react';
 // import { BrowserRouter as Router } from 'react-router-dom'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { DataProvider } from './GlobalState'
-import HeaderDev from './components/headers/Header'
+import Headers from './components/headers/Headers'
 import Header from './components/header/Header';
 import MainPages from './components/mainpages/Pages'
 import Home from './components/home/Home.js';
+import Vendors from './components/vendor/Vendors';
+import Login from './components/mainpages/auth/Login';
 
 function App() {
   return (
+    <DataProvider>
     <Router>
-      <Switch>
-        <Route path="/home">
-          <Header />
-          <Home />
-        </Route>
-        {/* <Route path="/home">
-          <Header />
-          <Home />
-        </Route> */}
-        <DataProvider>
-          <Route path="/">
-            {/* <Header /> */}
-            <div className="App">
-              <HeaderDev />
-              <MainPages />
-            </div>
-          </Route>
-        </DataProvider>
-      </Switch>
+
+      <div className="App">
+        <MainPages />
+      </div>
     </Router>
+  </DataProvider>
   );
 }
 
